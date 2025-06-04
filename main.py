7d6ddc70abca9779713aba30ade340b37c9c46f3
RodCrvlh <digaorrvcc@gmail.com>
2025-06-04 00:31:47 -0300

    correcao dos metodos atualizar e da quantidade de pagina e quadro

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 # classe da Tabela de Paginas
 
-import tkinter
+# import tkinter
 import math
 
 
@@ -14,6 +14,7 @@ class TP:
         self.tam_endereco = tam_endereco
         self.entradas = self.init_entradas()
 
+    # inicializa o dicionario entradas que representa as entradas da TP
     def init_entradas(self):
         entradas = []
         for _ in range(self.n_entradas):
@@ -33,11 +34,16 @@ class TP:
             return -1  # nao acha a pagina e ativa a Politica de Substituicao
 
     # atualiza uma entrada da TP
-    def atualizar(self, n_pagina, p, m, n_quadro):
-        self.entradas[n_pagina]['Presenca'] = p
-        self.entradas[n_pagina]['Modificacao'] = m
-        self.entradas[n_pagina]['Quadro'] = n_quadro
+    def atualizar(self, n_pagina, p, m, n_quadro, GM):
+        for entrada in self.entradas:
+            if entrada['Presenca'] == 0:
+                entrada['Presenca'] = 1
+                entrada['Modificacao'] = m
+                entrada['Quadro'] = n_quadro
+            else:
+                GM.politica_substituicao()
 
+    # mostra o estado atual da TP
     def mostrar(self):
         for item in self.entradas:
             print(item)
@@ -51,7 +57,7 @@ class TLB:
         self.tam_endereco = tam_endereco
         self.entradas = self.init_entradas()
 
-
+    # inicializa o dicionario entradas que representa as entradas da TLB
     def init_entradas(self):
         entradas = []
         for _ in range(self.n_entradas):
@@ -65,28 +71,40 @@ class TLB:
             entradas.append(entrada)
         return entradas
 
+    # Busca nas entradas da TLB uma pagina especifica e também ve se é válida a pagina
     def buscar(self, n_pagina):
-        for _ in range(self.n_entradas):
-            if self.entradas['Pagina'] == n_pagina:
-                return self.entradas['Quadro']
+        for entrada in self.n_entradas:
+            if entrada['Validade'] == 1 and entrada['Pagina'] == n_pagina:
+                return self.entradas[n_pagina]['Quadro']
         return -1
 
-    def atualizar(self, n_pagina_antiga, n_pagina_nova, v, p, m, n_quadro):
-        i = 0
-        while i < self.n_entradas:
-            if self.entradas['Pagina'] == n_pagina_antiga:
-                self.entradas[n_pagina_antiga]['Validade'] = v
-                self.entradas[n_pagina_antiga]['Pagina'] = n_pagina_nova
-                self.entradas[n_pagina_antiga]['Presenca'] = p
-                self.entradas[n_pagina_antiga]['Modificacao'] = m
-                self.entradas[n_pagina_antiga]['nQuadro'] = n_quadro
+    # Atualiza uma entrada da TLB
+    # Prec
+    def atualizar(self, n_pagina, v, p, m, n_quadro, GM):
+        for entrada in self.entradas:
+            if entrada['Presenca'] == 0:
+                entrada['Presenca'] = 1
+                entrada['Validade'] = v
+                entrada['Pagina'] = n_pagina
+                entrada['Presenca'] = p
+                entrada['Modificacao'] = m
+                entrada['nQuadro'] = n_quadro
+                break
             else:
-                i = i + 1
+                GM.politica_substituicao(self)
 
     def mostrar(self):
         for item in self.entradas:
             print(item)
 
+
+class GerenciadorMemoria:
+
+    # inicializa uma entrada do GerenciadorMemoria
+    def __int__(self):
+
+     def politica_substituicao():
+         return 0
 
 
 class MemoriaFisica:
@@ -98,37 +116,66 @@ class MemoriaFisica:
 
 
 def main():
-    n_quadro = int(input("Defina o numero de quadros:"))
-    n_pagina = int(input("Defina o numero de paginas"))
-    tam_quadro = (input("Defina o tamanho do quadro/página:"))
+    tam_quadro = int(input("Defina o tamanho do quadro/página:"))
     tam_end_logico = int(input("Digite o tamanho do endereço lógico:"))
     n_entrada = int(input("Digite o numero de entradas da TLB:"))
+    tam_mem_fisica = int(input("Digite o tamanho da memoria fisica:"))
+    tam_mem_sec = int(input("Digite o tamanho da memoria secundaria:"))
 
-    cond = 0
+    while True:
 
-    while cond != 1 or cond != 2:
-        try:
-            cond = int(
-                input("Digite 1 para memória secundária ilimitada ou 2 para memória secundária definida: "))
-        except ValueError:
-            print("Por favor, digite um número válido (1 ou 2).")
+        cond = int(input("Digite 1 para memória secundária ilimitada ou 2 para memória secundária definida: "))
 
-    mem_secundaria = False
+        if cond == 1:
+            mem_secundaria = True
+            break
 
-    if cond == 1:
-        mem_secundaria = True
-    elif cond == 2:
-        mem_secundaria = False
+        elif cond == 2:
+            mem_secundaria = False
+            break
+
+        else:
+            print("Por favor, digite 1 ou 2.")
+
+    n_quadro = tam_mem_fisica / tam_quadro
+
+    # tamanho do end logico esta em bit e o tmanho da pagina que é igual ao quadro esta em byte
+    n_pagina = math.pow(2, tam_end_logico) / tam_quadro * 4
 
     # 1 bit de presenca e 1 de modificacao
     # Usamos log na base 2 porque com 1024 quadros por exemplo, poderemos ter 10 bits para endereçar os quadros
     tam_entrada_tp = 2 + math.log(n_quadro, 2)
+
     # 1bit de presenca, 1 bit de modificacao e 1 bit de validade
     # mesma logica do log mas agora usada tambem para o numero de paginas
-    tam_entrada_tlb = 3 + math.log(n_quadro, 2)+ math.log(n_pagina, 2)
+    tam_entrada_tlb = 3 + math.log(n_quadro, 2) + math.log(n_pagina, 2)
+
     tp = TP(n_entrada, tam_entrada_tp)
     tlb = TLB(n_entrada, tam_entrada_tlb)
     mem_fisica = MemoriaFisica(n_quadro, tam_quadro)
+    GM = GerenciadorMemoria()
+
+    while True:
+        print("-------------------------------------------------")
+        print("Bem vindo ao simulador de gerenciador de memoria! Digte o numero referente a opcao.")
+        print("1- Adcionar entrada na TP")
+        print("2- Adcionar entrada na TLB")
+        print("3- Sair")
+        print("-------------------------------------------------")
+
+        opcao = input()
+
+        if opcao == 1:
+            tp.atualizar(n_entrada, tam_entrada_tp, GM)
+
+        elif opcao == 2:
+            tlb.atualizar(n_entrada, tam_entrada_tlb, GM)
+
+        elif opcao == 3:
+            break
+
+        else:
+            print("Digite um numero referente as opcoes.")
 
 
 if __name__ == "__main__":
