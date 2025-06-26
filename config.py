08e2cef4cd7bbc18087788ad71ded0cc1a3dadf0
Alexsander Teixeira <alexsandersoarest@gmail.com>
2025-06-26 18:34:48 -0300

    removed useless stuff and renamed frame table

--- config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    def __init__(self, tam_mp, tam_ms, tam_quadro):
-        self.tam_mp = tam_mp
-        self.tam_ms = tam_ms
-        self.tam_quadro = tam_quadro
-        self.tam_pagina = tam_quadro
